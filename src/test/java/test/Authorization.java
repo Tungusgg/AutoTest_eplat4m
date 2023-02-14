5d344012bfd53b278d479a7d0aa511fc5c3b53bd
Tunguss <matygin.nekit.02@yandex.ru>
2023-02-14 18:34:33 +0700

    GgWpssdf

--- src/test/java/test/Authorization.java
+++ src/test/java/test/Authorization.java
@@ -8,24 +8,27 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.Random;
 public class Authorization extends SetUpp{
-    @Test
-    public void Negative(){
-        //Введение невалидных данных в поля
-        driver.get("https://grc.eplat4m.ru/");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@autofocus=\"True\"]")));
-        Random random = new Random();
-        int n = random.nextInt(100) + 1;
-        String name = "САПУИБ\\NotExist" + n ;
-        System.out.println(name);
-        driver.findElement(By.xpath("//input[@autofocus=\"True\"]")).sendKeys(name);
-        String passw = "wer" + n + "gg" + n;
-        System.out.println(passw);
-        driver.findElement(By.xpath("//input[@type=\"password\"]")).sendKeys(passw);
-        driver.findElement(By.xpath("//input[@type=\"checkbox\"]")).click();
-        String title = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//option[@value=contains(text(), \"Локальный вход\")]"))).getText();
-        boolean ttss = title.equals("Локальный вход");
-        while (ttss != true){
-            driver.findElement(By.xpath("//option[@value=contains(text(), \"Локальный вход\")]")).click();
+    String user = "//input[@autofocus=\"True\"]";
+    String password = "//input[@type=\"password\"]";
+    String checkbox = "//input[@type=\"checkbox\"]";
+    String linput = "//option[@value=contains(text(), \"Локальный вход\")]";
+    String buttn = "//button[@id=\"btnLogin\"]";
+    String url = "https://grc.eplat4m.ru/";
+
+    public void findSendkey(String names, String name1){//Нахождение элемента со вставкой текста(X.Path)
+        driver.findElement(By.xpath(names)).sendKeys(name1);
+    }
+
+    public void findClick(String names){//Нахождение + клик по элементу(X.Path)
+        driver.findElement(By.xpath(names)).click();
+    }
+    public void waitVisibilityElement(String names){//Ожидание элемента(X.Path)
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(names)));
+    }
+
+    public void checkingValue(Boolean valuess, String names, String title){//Проверка на совпадение элемента(X.Path)
+        while (valuess != true){
+            findClick(linput);
             new Actions(driver)
                     .keyDown(Keys.DOWN)
                     .sendKeys("")
@@ -34,41 +37,48 @@ public class Authorization extends SetUpp{
                     .keyDown(Keys.ENTER)
                     .sendKeys("")
                     .perform();
-            title = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//option[@value=contains(text(), \"Локальный вход\")]"))).getText();
-            ttss = title.equals("Локальный вход");
+            title = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(linput))).getText();
+            valuess = title.equals("Локальный вход");
         }
-        driver.findElement(By.xpath("//button[@id=\"btnLogin\"]")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class=\"validation-summary-errors\"]//ul//li[contains(text(), \"Неправильный способ входа, имя пользователя или пароль.\")]")));
+    }
+
+    @Test
+    public void Negative(){
+        //Введение невалидных данных в поля
+        driver.get(url);
+        waitVisibilityElement(user);
+        Random random = new Random();
+        int n = random.nextInt(100) + 1;
+        String name = "САПУИБ\\NotExist" + n ;
+        System.out.println(name);
+        findSendkey(user, name);
+        String passw = "wer" + n + "gg" + n;
+        System.out.println(passw);
+        findSendkey(password, passw);
+        findClick(checkbox);
+        String title = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(linput))).getText();
+        boolean ttss = title.equals("Локальный вход");
+        checkingValue(ttss, linput, title);
+        findClick(buttn);
+        waitVisibilityElement("//div[@class=\"validation-summary-errors\"]//ul//li[contains(text(), \"Неправильный способ входа, имя пользователя или пароль.\")]");
         driver.navigate().refresh();
     }
     @Test
     public void Passitive(){
         //Введение валидных данных в поля
-        driver.get("https://grc.eplat4m.ru/");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@autofocus=\"True\"]")));
-        driver.findElement(By.xpath("//input[@autofocus=\"True\"]")).sendKeys("САПУИБ\\test2");
-        driver.findElement(By.xpath("//input[@type=\"password\"]")).sendKeys("654321");
-        driver.findElement(By.xpath("//input[@type=\"checkbox\"]")).click();
-        String title = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//option[@value=contains(text(), \"Локальный вход\")]"))).getText();
+        driver.get(url);
+        waitVisibilityElement(user);
+        findSendkey(user, "САПУИБ\\test2");
+        findSendkey(password, "654321");
+        findClick(checkbox);
+        String title = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(linput))).getText();
         boolean ttss = title.equals("Локальный вход");
-        while (ttss != true){
-            driver.findElement(By.xpath("//option[@value=contains(text(), \"Локальный вход\")]")).click();
-            new Actions(driver)
-                    .keyDown(Keys.DOWN)
-                    .sendKeys("")
-                    .perform();
-            new Actions(driver)
-                    .keyDown(Keys.ENTER)
-                    .sendKeys("")
-                    .perform();
-            title = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//option[@value=contains(text(), \"Локальный вход\")]"))).getText();
-            ttss = title.equals("Локальный вход");
-        }
-        driver.findElement(By.xpath("//button[@id=\"btnLogin\"]")).click();
+        checkingValue(ttss, linput, title);
+        findClick(buttn);
         //Проверка активности вкладки "Соотвествие требованиям" по нахождению элемента на этой странице
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[@class=contains(text(), \" несоответствий по источникам требований\")]")));
-        driver.findElement(By.xpath("(//div[@class=\"dashboardControl grc-dashboard-link grc-dashboard-Link\"]//a)[11]")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div//label[@class=contains(text(), \"Проекты, которым необходимо назначить аудитора\")])[last()]")));
+        waitVisibilityElement("//label[@class=contains(text(), \" несоответствий по источникам требований\")]");
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class=\"dashboardControl grc-dashboard-link grc-dashboard-Link\"]//a)[11]"))).click();
+        waitVisibilityElement("(//div//label[@class=contains(text(), \"Проекты, которым необходимо назначить аудитора\")])[last()]");
     }
 
 }
--- src/test/java/test/SetUpp.java
+++ src/test/java/test/SetUpp.java
@@ -11,13 +11,14 @@ import java.time.Duration;
 public class SetUpp {
     public WebDriver driver;
     public WebDriverWait wait;
+
     @Before
     public void setUp(){
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(25));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(40));
     }
     @After
     public void close(){
-        //driver.quit();
+        driver.quit();
     }
 }
